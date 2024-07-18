13f4872d5720ef43e892b17b2e182556e8f4526c
azinorishironingen <aoi.taguchi.m@gmail.com>
2024-07-18 19:59:42 +0900

    kazu

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask import render_template
 import datetime
 
 app = Flask(__name__)
+
 @app.route("/")
 def top():
     return str(datetime.datetime.now())
+
+@app.route("/kazu")
+def kazu():
+    return render_template("signup.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
 app.run(host="0.0.0.0")
